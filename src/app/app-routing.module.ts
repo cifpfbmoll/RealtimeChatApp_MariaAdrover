3900b128dbad27bca144851fb34f76f16ee1b270
Unknown <maria.adrover.v@gmail.com>
2019-02-07 11:37:00 +0100

    Part 1: ex. 0-3
    Structure + login component + signup component + navBar component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from 'src/app/pages/login/login.component';
+import { SignupComponent } from 'src/app/pages/signup/signup.component';
+import { ChatComponent } from 'src/app/pages/chat/chat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: '/login'},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup',  component: SignupComponent},
+  {path: 'chat',  component: ChatComponent},
+  {path:'**', redirectTo: '/login'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ChatComponent } from './pages/chat/chat.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    NavbarComponent,
+    ChatComponent
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
