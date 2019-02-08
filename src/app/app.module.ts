8a3d112aeb27c98be901c6c66d37a5e7f927f3f3
Maria Adrover <maria.adrover.v@gmail.com>
2019-02-08 19:49:45 +0100

    Fixed chat-message.component.html.  chat-input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +9,11 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ChatComponent } from './pages/chat/chat.component';
+import { ChatInputComponent } from './pages/chat/components/chat-input/chat-input.component';
+import { ChatroomListComponent } from './pages/chat/components/chatroom-list/chatroom-list.component';
+import { ChatroomTitleBarComponent } from './pages/chat/components/chatroom-title-bar/chatroom-title-bar.component';
+import { ChatMessageComponent } from './pages/chat/components/chat-message/chat-message.component';
+import { ChatroomWindowComponent } from './pages/chat/components/chatroom-window/chatroom-window.component';
 
 
 @NgModule({
@@ -17,10 +22,16 @@ import { ChatComponent } from './pages/chat/chat.component';
     LoginComponent,
     SignupComponent,
     NavbarComponent,
-    ChatComponent
+    ChatComponent,
+    ChatInputComponent,
+    ChatroomListComponent,
+    ChatroomTitleBarComponent,
+    ChatMessageComponent,
+    ChatroomWindowComponent
   ],
   imports: [
     ReactiveFormsModule,
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     BsDropdownModule.forRoot()
